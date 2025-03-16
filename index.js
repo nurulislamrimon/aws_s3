51fc1686e7c6810e23b9d53f0c7783ba7dec4a82
nurulislamrimon <nurulislamrimon@gmail.com>
2025-03-16 21:19:10 +0600

    get presigned url function added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { config } from "dotenv";
+
+config();
+
+// create client using user credentials
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+// create a function to get signed url
+const getObjectCommand = async (key) => {
+  const command = new GetObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+  });
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+  return url;
+};
+
+// console.log(await getObjectCommand("myPhoto.jpg"));
